24dfd42aa1f44c5a0a34e9d98bbd15d4d737fb18
HEXIAYUE <q1506589520q@gmail.com>
2020-02-28 10:49:45 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,97 @@
+
+var input=document.getElementById("input");
+
+function my1()
+{
+  input.value = input.value+document.getElementById("a").value;
+
+}
+
+function my2() {
+  input.value = input.value+ document.getElementById("b").value ;
+
+}
+
+function my3() {
+  input.value = input.value + document.getElementById("c").value;
+
+}
+
+function my4() {
+  input.value = input.value + document.getElementById("d").value;
+
+}
+
+function my5() {
+  input.value = input.value + document.getElementById("e").value;
+
+}
+
+function my6() {
+  input.value = input.value + document.getElementById("f").value;
+
+}
+
+function my7() {
+  input.value = input.value + document.getElementById("g").value;
+
+}
+
+function my8() {
+  input.value = input.value + document.getElementById("h").value;
+
+}
+
+function my9() {
+  input.value = input.value + document.getElementById("i").value;
+
+}
+function my10() {
+  input.value = input.value + document.getElementById("j").value;
+
+}
+function my11() {
+  input.value = input.value + document.getElementById("k").value;
+
+}
+function my12() {
+  input.value = input.value + document.getElementById("l").value;
+
+}
+function my13() {
+  input.value = input.value + document.getElementById("m").value;
+
+}
+function my14() {
+  input.value = input.value + document.getElementById("p").value;
+
+}
+function AC()
+{
+  input.value="";
+}
+
+function T(){
+  input.value = input.value + document.getElementById("t").value;
+
+}
+
+function N() {
+  input.value = input.value + document.getElementById("n").value;
+
+}
+function DENG_YU()
+{
+  input.value=eval(input.value); 
+}
+
+
+
+
+
+
+
+
+
+
+
